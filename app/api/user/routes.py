238aa69f7a993ef0accc4985fd22d74fc49338b6
msp-corbett <m.s.p.corbett@hotmail.com>
2020-03-31 08:57:08 +1300

    Built patch and delete methods for User api. Corrected/removed route style as it interfered with http methods.

--- app/api/user/routes.py
+++ app/api/user/routes.py
@@ -9,16 +9,15 @@ from .models import User, UserSchema
 class UserView(FlaskView):
     trailing_slash = False
 
-    @route('')
-    @route('/<int:pk_id>')
-    def get(self, pk_id=None):
-        if pk_id:
-            schema = UserSchema()
-            data = db.session.query(User).filter((User.ID == pk_id)).first()
-        else:
-            schema = UserSchema(many=True)
-            data = db.session.query(User).all()
-        
+    def index(self,):
+        schema = UserSchema(many=True)
+        data = db.session.query(User).all()
+        return schema.jsonify(data)
+
+
+    def get(self, pk_id):
+        schema = UserSchema()
+        data = db.session.query(User).filter((User.ID == pk_id)).first()
         return schema.jsonify(data)
 
 
@@ -64,17 +63,66 @@ class UserView(FlaskView):
         return data, response
 
 
-    def put(self,):
+    def put(self, pk_id):
         """ PUT not supported on model that generates incremental Primary Key """
         return {"message": "method not allowed."}, 405
 
     
-    @route('/<int:pk_id>')
     def patch(self, pk_id):
-        pass
+        record_query = db.session.query(User).filter((User.ID == pk_id)).first()
+        schema = UserSchema()
+
+        # User agent should know target resource via GET request.
+        if not record_query:
+            return {"message": "Requested user not found"}, 404
+        
+        request_data = request.get_json(force=True, silent=True)
+        if not request_data:
+            return {'message': 'No content provided.'}, 400
+
+        # Build patch from supplied data
+        apply_patch = {}
+        allowed_operations = ("replace")
+        for patch_data in request_data:
+            try:
+                if patch_data.get("op").lower() not in allowed_operations:
+                    return {'message': f'Operation "{patch_data.get("op")}" not supported.'}, 400
+            except AttributeError:
+                return {'message': f'Invalid operation {patch_data.get("op")}.'}, 400
+
+            apply_patch.update({patch_data.get('path'): patch_data.get("value")})
+
+        # Validate request data
+        try:
+            schema.load(apply_patch)
+        except ValidationError as err:
+            return {'message': err.messages}, 400
+
+        try:
+            for k, v in apply_patch.items():
+                setattr(record_query, k, v)
+            db.session.commit()
+            data, code = {'message': f"User updated."}, 200
+        except (exc.SQLAlchemyError, TypeError):
+            db.session.rollback()
+            data, code = {"message": "Record could not be updated."}, 500
+
+        return data, code
 
     
-    @route('/<int:pk_id>')
-    def delete(self): 
-        pass
+    def delete(self, pk_id): 
+        user = db.session.query(User).filter((User.ID == pk_id)).first()
+
+        if user:
+            try:
+                db.session.delete(user)
+                db.session.commit()
+                data, response = {"message": "record deleted."}, 200
+            except exc.SQLAlchemyError:
+                db.session.rollback()
+                data, response = {"message": "Record could not be deleted."}, 500
+        else:
+            data, response = {"message": "No matching user."}, 404
+        
+        return data, response
 
